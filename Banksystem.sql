ec092748093376edb0280dcc8b49e6dbe74c84d2
Codersabina199 <157024829+Codersabina199@users.noreply.github.com>
2024-01-25 02:11:22 -0800

    Add files via upload

---
+++ Banksystem.sql
@@ -0,0 +1,8 @@
+create database bankSystem;
+use bankSystem;
+create table signups(
+form_no varchar(30),name varchar(30),father_name varchar(30),
+address varchar(60),DOB varchar(30),gender varchar(30),
+email varchar(60),marital_status varchar(30),city varchar(30),
+pincode varchar(30));
+select * from signups;
